db742d18f027b5db1028c61f20d67bc0515d981a
ukrainskiy40 <ukrainsky@kaluga.ru>
2022-11-23 09:35:00 +0300

    first commit

---
+++ src/main/java/ru/ukrainskiy/rnd/order/Order.java
@@ -0,0 +1,32 @@
+package ru.ukrainskiy.rnd.order;
+
+public class Order {
+
+    private Long id;
+    // other field
+    // getters and setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+
+        Order order = (Order) o;
+
+        return getId() != null ? getId().equals(order.getId()) : order.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
---
+++ src/main/java/ru/ukrainskiy/rnd/order/OrderService.java
@@ -0,0 +1,100 @@
+package ru.ukrainskiy.rnd.order;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderService {
+
+    /**
+     * Сервис обработки order в многопоточной среде
+     */
+    private final ReentrantReadWriteLock.ReadLock readLock;
+    private final ReentrantReadWriteLock.WriteLock writeLock;
+
+    private final Map<Long, Order> orders = new ConcurrentHashMap<>();
+    private final Deque<Order> latest = new ConcurrentLinkedDeque<>();
+
+    public OrderService() {
+        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        this.readLock = readWriteLock.readLock();
+        this.writeLock = readWriteLock.writeLock();
+    }
+
+    /**
+     * Верент все order отсортирование в обратном порядке добавления
+     * @return List<Order>
+     */
+    public List<Order> findAllOrder() {
+        try {
+            readLock.lock();
+            return orders.values().stream().sorted((o1, o2) -> Long.compare(o2.getId(), o1.getId())).collect(Collectors.toList());
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    /**
+     * Вернет order по его id
+     * @param id
+     * @return Optional<Order>
+     */
+    public Optional<Order> findOrderById(Long id) {
+        try {
+            readLock.lock();
+            return Optional.ofNullable(orders.get(id));
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    /**
+     * Добавить новый order
+     * @param order
+     */
+    public void addOrder(Order order) {
+        try {
+            writeLock.lock();
+            orders.put(order.getId(), order);
+            if (latest.size() == 100) {
+                latest.removeLast();
+            }
+            latest.addFirst(order);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    /**
+     * Удалить order
+     * @param order
+     */
+    public void removeOrder(Order order) {
+        try {
+            writeLock.lock();
+            orders.remove(order.getId());
+            latest.remove(order);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    /**
+     * Вернуть последние 100 добавленных orders
+     * @return
+     */
+    public List<Order> lastOrder100() {
+        try {
+            readLock.lock();
+            return new ArrayList<>(latest);
+        } finally {
+            readLock.unlock();
+        }
+    }
+}
